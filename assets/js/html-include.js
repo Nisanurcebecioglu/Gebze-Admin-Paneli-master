3dd1ab571fbdf817e17060d6cb5d1cc0afa43c3d
Nisanurcebecioglu <nisanurcebecioglu@trakya.edu.tr>
2025-08-22 14:45:15 +0300

    Update html-include.js

--- assets/js/html-include.js
+++ assets/js/html-include.js
@@ -20,9 +20,34 @@
             }
             if (window.jQuery && window.$) {
                 try {
+                    // Sidebar menü başlatma
                     if (typeof $.sidebarMenu === 'function') {
-                        $.sidebarMenu($('.sidebar-menu'));
+                        $('.sidebar-menu').off('click.sidebarMenu').on('click.sidebarMenu', 'li a', function (e) {
+                            var $this = $(this);
+                            var checkElement = $this.next();
+                            var subMenuSelector = '.sidebar-submenu';
+
+                            if (checkElement.is(subMenuSelector) && checkElement.is(':visible')) {
+                                checkElement.slideUp(300, function () {
+                                    checkElement.removeClass('menu-open');
+                                });
+                                checkElement.parent("li").removeClass("active");
+                            }
+                            else if ((checkElement.is(subMenuSelector)) && (!checkElement.is(':visible'))) {
+                                var parent = $this.parents('ul').first();
+                                var ul = parent.find('ul:visible').slideUp(300);
+                                ul.removeClass('menu-open');
+                                var parent_li = $this.parent("li");
+                                checkElement.slideDown(300, function () {
+                                    checkElement.addClass('menu-open');
+                                    parent.find('li.active').removeClass('active');
+                                    parent_li.addClass('active');
+                                });
+                            }
+                        });
                     }
+
+                    // Header toggle
                     var $nav = $('.page-sidebar');
                     var $header = $('.page-main-header');
                     var $toggle = $('#sidebar-toggle');
@@ -32,6 +57,8 @@
                             $header.toggleClass('open');
                         });
                     }
+
+                    // Responsive
                     var w = $(window).width();
                     if (w + 17 <= 991) {
                         $toggle.addClass('open');
